1b452924dbd7b57d6f03f34a2ca5aa92fe71c90a
Kickblip <wyatt.zilker@gmail.com>
2024-03-18 19:23:37 -0600

    lab 8

---
+++ autocorr.py
@@ -0,0 +1,18 @@
+# autocorr
+# Compute cyclic autocorrelation of x.
+#
+# INPUT:
+# x: A 1D array with d entries.
+#
+# OUTPUTS:
+# X: The cycle autocorrelation of x. A 1D array with d entries.
+#
+# AUTHOR:
+# Emily J. King
+
+import numpy as np
+#from scipy.fft import fft
+
+def autocorr(x):
+    X=np.real(np.fft.ifft(np.conj(np.fft.fft(x))*np.fft.fft(x)))
+    return(X)
